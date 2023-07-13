f344dbb02f9eb7f3c8af43c18d5eb37801512971
Glen Maddern <glen@cloudflare.com>
2023-07-13 13:42:36 +1000

    this might be enough for now

--- test/zest.test.ts
+++ test/zest.test.ts
@@ -3,20 +3,27 @@ import z from 'zod'
 
 import * as zest from '../src'
 
-const ok = (fn: Function) => fn()
-const err = (
-  fn: Function,
+const ok = <T, U>(a: T, b: U) => zest.expect(a).toMatchObject(b)
+const err = <T, U>(
+  a: T,
+  b: U,
   expected?: string | Constructable | RegExp | Error
-) => expect(fn).toThrow(expected)
+) => expect(() => zest.expect(a).toMatchObject(b)).toThrow(expected)
 
 describe('zest', () => {
   it('should work', () => {
-    ok(() => zest.expect({ a: 1 }).toMatchObject({ a: 1 }))
-    err(() => zest.expect({ a: 1 }).toMatchObject({ a: 2 }))
+    ok({ a: 1 }, { a: 1 })
+    err({ a: 1 }, { a: 2 })
 
-    ok(() => zest.expect({ a: 1, b: 2 }).toMatchObject({ a: 1 }))
-    err(() => zest.expect({ a: 1 }).toMatchObject({ a: 1, b: 2 }))
+    ok({ a: 1, b: 2 }, { a: 1 })
+    err({ a: 1 }, { a: 1, b: 2 })
 
-    ok(() => zest.expect({ a: 1, b: 2 }).toMatchObject({ a: 1, b: z.number() }))
+    ok({ a: 1, b: 2 }, { a: 1, b: z.number() })
+    ok({ a: 1, b: 2 }, { a: 1, b: z.number().gt(0) })
+    ok({ a: 1, b: 2 }, { a: 1, b: z.number().gte(2) })
+
+    err({ a: 1, b: 2 }, { a: 1, b: z.string() })
+    err({ a: 1, b: 2 }, { a: 1, b: z.number().lt(0) })
+    err({ a: 1, b: 2 }, { a: 1, b: z.number().gt(2) })
   })
 })
