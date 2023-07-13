3a56f573f4c94eb998fa0de792a6fb61f3b62511
Glen Maddern <glen@cloudflare.com>
2023-07-13 13:36:54 +1000

    weirdly easy to get going, now

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,33 @@
-import * as assert from 'node:assert'
+import z, { ZodType } from 'zod'
+import mapValues from 'just-map-values'
 
-function toZodObj(obj: Record<string, unknown>) {}
+function toZodValue(val: unknown): ZodType {
+  if (val === null) return z.never()
 
-function toZodValue(val: unknown) {
-  if (typeof val === 'string') {
+  if (val instanceof ZodType) return val
+
+  switch (typeof val) {
+    case 'undefined':
+      return z.never()
+    case 'string':
+    case 'number':
+    case 'bigint':
+    case 'boolean':
+      return z.literal(val)
+    case 'object': // except null, caught earlier
+      return z.object(
+        mapValues(val as Record<string, unknown>, (val) => toZodValue(val))
+      )
+    default: // function, symbol
+      throw new Error(`Can't infer zod validator from ${typeof val}: ${val}`)
   }
 }
 
 export function expect<T = unknown>(actual: unknown) {
-  const schema = toZodValue(actual)
   return {
     toMatchObject(expected: T) {
-      assert.deepEqual(actual, expected)
+      const schema = toZodValue(expected)
+      schema.parse(actual)
     },
   }
 }
--- test/zest.test.ts
+++ test/zest.test.ts
@@ -1,4 +1,5 @@
 import { describe, it, expect, Constructable } from 'vitest'
+import z from 'zod'
 
 import * as zest from '../src'
 
@@ -12,5 +13,10 @@ describe('zest', () => {
   it('should work', () => {
     ok(() => zest.expect({ a: 1 }).toMatchObject({ a: 1 }))
     err(() => zest.expect({ a: 1 }).toMatchObject({ a: 2 }))
+
+    ok(() => zest.expect({ a: 1, b: 2 }).toMatchObject({ a: 1 }))
+    err(() => zest.expect({ a: 1 }).toMatchObject({ a: 1, b: 2 }))
+
+    ok(() => zest.expect({ a: 1, b: 2 }).toMatchObject({ a: 1, b: z.number() }))
   })
 })
